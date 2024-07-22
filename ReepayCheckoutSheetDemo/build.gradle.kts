8359fa81f9f4d927fe40e3a5849bd6f22c298da4
Johnny <108516218+johnnyrpy@users.noreply.github.com>
2024-07-22 09:01:47 +0100

    chore(BP-1247): bump checkout sheet version number (#28)

--- ReepayCheckoutSheetDemo/build.gradle.kts
+++ ReepayCheckoutSheetDemo/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
 
     // Option 1. implement external dependency
-    implementation("com.github.reepay:reepay-android-checkout-sheet:1.0.7")
+    implementation("com.github.reepay:reepay-android-checkout-sheet:1.0.9")
 
 
     // Option 2. Implement local depenency
@@ -49,7 +49,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
-    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("com.google.code.gson:gson:2.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
