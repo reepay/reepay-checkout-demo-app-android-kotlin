86a3e9fad3144c4e4d699548b9ecc07b51e5e453
johnnyrpy <johnny@reepay.com>
2024-11-12 17:16:09 +0100

    feat(BRO-898): handle incoming returnUrl

--- ReepayCheckoutSheetDemo/src/main/java/com/billwerk/checkoutsheetdemo/IntentHandler.kt
+++ ReepayCheckoutSheetDemo/src/main/java/com/billwerk/checkoutsheetdemo/IntentHandler.kt
@@ -1,6 +1,7 @@
 package com.billwerk.checkoutsheetdemo
 
 import android.content.Intent
+import android.net.Uri
 import com.billwerk.checkout.CheckoutSheet
 import com.billwerk.checkout.CheckoutSheetConfig
 
@@ -9,8 +10,18 @@ class IntentHandler(private val checkoutSheet: CheckoutSheet) {
         if (Intent.ACTION_VIEW == intent.action) {
             val uri = intent.data
             if (uri != null) {
-                checkoutSheet.open(config)
+                val returnUrl = extractReturnUrl(uri.toString())
+                if (returnUrl != null) {
+                    checkoutSheet.presentCheckoutReturnUrl(config, returnUrl)
+                } else {
+                    checkoutSheet.open(config)
+                }
             }
         }
+    }
+
+    private fun extractReturnUrl(url: String): String? {
+        val uri = Uri.parse(url)
+        return uri.getQueryParameter("returnUrl")
     }
 }
