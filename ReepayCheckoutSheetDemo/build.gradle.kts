eb282cfedbe2eecd685dd797f06c14809b2c68a6
Asger Rud Hansen <112621087+asger-reepay@users.noreply.github.com>
2025-06-13 10:50:54 +0200

    fix(BRO-1155): increase touch target for close button (#79)

--- ReepayCheckoutSheetDemo/build.gradle.kts
+++ ReepayCheckoutSheetDemo/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
 
     // Option 1. implement external dependency
-    implementation("com.github.reepay:reepay-android-checkout-sheet:1.0.16")
+    implementation("com.github.reepay:reepay-android-checkout-sheet:1.0.18")
 
 
     // Option 2. Implement local dependency
